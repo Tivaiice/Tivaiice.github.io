90db07b8e9c3c116202300645ad6e882cb9877fb
tivaiice <tiva.wk@hotmail.com>
2021-04-23 23:30:57 +0700

    Add Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,56 +1,221 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "../styles/Project.css";
 import WakekyWay from "../images/WakekyWay.png";
 import Modify from "../images/Modify.png";
 import Pokemon from "../images/Pokemon.png";
 import Website from "../images/Website.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
 
 const txtProject = "</ PROJECT>";
 
 const Project = () => {
+  //WakekyWay
+  const openPopupboxWakekyWay = () => {
+    const content = (
+      <>
+        <img
+          className="project-image-WakekyWayPopupbox"
+          src="https://media.giphy.com/media/bwvloUaCoGXBF3ileD/giphy.gif"
+          alt="WakekyWay Clone Project..."
+        />
+        <p>Project AppWakekyWay แอปพลิเคชั่นแจ้งเตือนก่อนถึงที่หมาย</p>
+        <b>GitHub : </b>
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open("https://github.com/Tivaiice/FinalProject_AppWakekyWay")
+          }
+        >
+          https://github.com/Tivaiice/FinalProject_AppWakekyWay
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const openPopupboxConfigWakekyWay = {
+    titleBar: {
+      enable: true,
+      text: "FinalProject AppWakekyWay",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
+  //Modify
+  const openPopupboxModify = () => {
+    const content = (
+      <>
+        <img
+          className="project-image-ModifyPopupbox"
+          src={Modify}
+          alt="Modify Clone Project..."
+        />
+        <p>AppModify โชว์สินค้ารายการแต่งรถ</p>
+        <b>GitHub : </b>
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open("https://github.com/Tivaiice/AppModifyShop")
+          }
+        >
+          https://github.com/Tivaiice/AppModifyShop
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const openPopupboxConfigModify = {
+    titleBar: {
+      enable: true,
+      text: "App Modify",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
+  //Pokemon
+  const openPopupboxPokemon = () => {
+    const content = (
+      <>
+        <img
+          className="project-image-PokemonPopupbox"
+          src={Pokemon}
+          alt="Modify Clone Project..."
+        />
+        <p>App Pokemon โชว์รายละเอียดของPokemon</p>
+        <b>GitHub : </b>
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open("https://github.com/Tivaiice/Pokemon_Search")
+          }
+        >
+          https://github.com/Tivaiice/Pokemon_Search
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const openPopupboxConfigPokemon = {
+    titleBar: {
+      enable: true,
+      text: "App Pokemon",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
+  //Website
+  const openPopupboxWebsite = () => {
+    const content = (
+      <>
+        <img
+          className="project-image-WebsitePopupbox"
+          src={Website}
+          alt="Modify Clone Project..."
+        />
+        <p>Website</p>
+        <b>GitHub : </b>
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open("https://github.com/Tivaiice/Pokemon_Search")
+          }
+        >
+          https://github.com/Tivaiice/Pokemon_Search
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const openPopupboxConfigWebsite = {
+    titleBar: {
+      enable: true,
+      text: "Website",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
   return (
-    <div className="project-wrapper">
+    <div id="project" className="project-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">{txtProject}</h1>
         <div className="image-box-wrapper row justify-content-center">
-          <div className="project-image-box">
+          <div
+            className="project-image-box-WakekyWay"
+            onClick={openPopupboxWakekyWay}
+          >
             <img
-              className="project-image"
-              src={Website}
-              alt="WakekyWay Project..."
+              className="project-image-WakekyWay"
+              src={WakekyWay}
+              alt="WakekyWay Clone Project..."
             />
-            <div ClassName="overflow"></div>
+            <div className="overflow-WakekyWay"></div>
+            <FontAwesomeIcon className="project-icon-WW" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="project-image-box">
+          <div
+            className="project-image-box-Modify"
+            onClick={openPopupboxModify}
+          >
             <img
-              className="project-image"
-              src={Website}
+              className="project-image-Modify"
+              src={Modify}
               alt="Modify Project..."
             />
-            <div ClassName="overflow"></div>
+            <div className="overflow-Modify"></div>
+            <FontAwesomeIcon
+              className="project-icon-Modify"
+              icon={faSearchPlus}
+            />
           </div>
           {/* - */}
-          <div className="project-image-box">
+          <div
+            className="project-image-box-Pokemon"
+            onClick={openPopupboxPokemon}
+          >
             <img
-              className="project-image"
-              src={Website}
+              className="project-image-Pokemon"
+              src={Pokemon}
               alt="Pokemon Project..."
             />
-            <div ClassName="overflow"></div>
+            <div className="overflow-Pokemon"></div>
+            <FontAwesomeIcon
+              className="project-icon-Pokemon"
+              icon={faSearchPlus}
+            />
           </div>
           {/* - */}
-          <div className="project-image-box">
+          <div
+            className="project-image-box-Website"
+            onClick={openPopupboxWebsite}
+          >
             <img
-              className="project-image"
+              className="project-image-Website"
               src={Website}
               alt="Website Project..."
             />
-            <div ClassName="overflow"></div>
+            <div className="overflow-Website"></div>
+            <FontAwesomeIcon
+              className="project-icon-Website"
+              icon={faSearchPlus}
+            />
           </div>
-          {/* - */}
         </div>
       </div>
+      <PopupboxContainer {...openPopupboxConfigWakekyWay} />
+      <PopupboxContainer {...openPopupboxConfigModify} />
+      <PopupboxContainer {...openPopupboxConfigPokemon} />
+      <PopupboxContainer {...openPopupboxConfigWebsite} />
     </div>
   );
 };
