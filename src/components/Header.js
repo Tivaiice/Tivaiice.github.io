fdfdb1e867142c98208bd9c8f5dbb04ea43d7a58
tivaiice <tiva.wk@hotmail.com>
2021-04-28 02:09:36 +0700

    Update Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
           Hi, There
           <img
             src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif"
-            width="100px"
+            width="80px"
             className="githand"
             alt="..."
           />
